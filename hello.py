44bfdabbf1c664a7cbef83ed7eb62799c66d11c0
Ambish Yaqoob <ambishyaqoob2005@outlook.com>
2026-04-15 23:56:34 +0500

    Update greeting and add age prompt

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-name = input("Enter your name: ")
-print("Hello", name)
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print("Hello World!")
+print("Nice to meet you,", name)
+print("Your age is", age)
+
