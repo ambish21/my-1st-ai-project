1510f8d93f7feb0c36480183a0630c4f0612dd7b
Ambish Yaqoob <ambishyaqoob2005@outlook.com>
2026-04-16 22:24:18 +0500

    Update location.py

--- location.py
+++ location.py
@@ -1,1 +1,12 @@
+import requests
+
+def get_location():
+    url = "http://ip-api.com/json/"
+    response = requests.get(url)
+    data = response.json()
+
+    print("City:", data['city'])
+    print("Country:", data['country'])
+
+get_location()
 
