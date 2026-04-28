55e14ffe9644b3e4c47f844480b3807c4b5b34a2
Ambish21 <ambishyaqoob2005@outlook.com>
2026-04-28 23:00:17 +0500

    make modern calculator

--- scripts.js
+++ scripts.js
@@ -19,3 +19,12 @@ function calculate() {
     display.value = "Error";
   }
 }
+document.addEventListener("keydown", function(e) {
+  if (!isNaN(e.key) || "+-*/.%".includes(e.key)) {
+    append(e.key);
+  } else if (e.key === "Enter") {
+    calculate();
+  } else if (e.key === "Backspace") {
+    deleteLast();
+  }
+});
